cc122091c51f553f2a8f317de6821c73917d455c
IdealDestructor <idealdestructor@gmail.com>
2025-03-07 23:43:16 +0800

    Update vite.config.prod.js

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -1,32 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        visualizer({
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-        }),
-    ],
-    build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    'vue-vendor': ['vue', 'vue-router', 'pinia'],
-                    ui: ['@fortawesome/fontawesome-free'],
-                },
-            },
-        },
+  base: "/aiHub/", // 显式指定基础路径
+  plugins: [
+    vue(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
     },
-})
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "vue-vendor": ["vue", "vue-router", "pinia"],
+          ui: ["@fortawesome/fontawesome-free"],
+        },
+      },
+    },
+  },
+});
